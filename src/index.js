ebcb3ba404905eb9df85556cd54b5be8107b0081
nitishsharma <mydna@gmail.com>
2023-10-20 11:52:51 +0530

    two files deleted

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals"; 
 import App from "./App";  
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -9,4 +8,4 @@ root.render(
     <App></App>
   </>
 );
-reportWebVitals();
+ 
